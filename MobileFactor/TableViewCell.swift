427199d1151c8b089912115eda00f48dfa599db2
Vito D'Acunto <vitodacunto@10-24-48-15.0200.sal.it.iosda.org>
2017-03-07 10:03:49 +0100

    2017 07 03

--- MobileFactor/TableViewCell.swift
+++ MobileFactor/TableViewCell.swift
@@ -20,6 +20,8 @@ class TableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        icon.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
